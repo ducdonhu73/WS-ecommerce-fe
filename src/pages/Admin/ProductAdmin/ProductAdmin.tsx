0a9081e3607088dead467e8880d28973d1239d9a
DwcManhPTIT <ducmanhcr7ngt@gmail.com>
2023-04-23 17:52:25 +0700

    manhupdate

--- src/pages/Admin/ProductAdmin/ProductAdmin.tsx
+++ src/pages/Admin/ProductAdmin/ProductAdmin.tsx
@@ -7,11 +7,22 @@ import { useProduct } from "queries/productQueries";
 import { ProductResponse } from "apis/products/product.model";
 import { PrimaryButton } from "components";
 import { useNavigate } from "react-router";
+import ReactPaginate from 'react-paginate';
 
 function ProductAdmin() {
   const [listProduct, setListProduct] = useState<ProductResponse[]>([...Array<ProductResponse>(5)]);
   const navigate = useNavigate();
   const { mutate: getProduct } = useProduct();
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 6;
+  console.log(listProduct.length);
+
+  const pageCount = Math.ceil(listProduct.length / pageSize); // tính số trang cần phân trang
+  const handlePageClick = (pageNumber: { selected: number; }) => {
+    setCurrentPage(pageNumber.selected + 1);
+  };
+
+  const displayedProducts = listProduct.slice((currentPage - 1) * pageSize, currentPage * pageSize); // lấy các sản phẩm tương ứng với trang hiện tại
 
   useEffect(() => {
     getProduct(
@@ -56,13 +67,28 @@ function ProductAdmin() {
       </div>
       {/* list offer */}
       <div>
-        {listProduct.map((item, index) => {
+        {displayedProducts.map((item, index) => {
           return (
             <div key={index}>
               <ListItem product={item} />
             </div>
           );
         })}
+        <ReactPaginate
+        pageCount={pageCount}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        onPageChange={handlePageClick}
+        // containerClassName={'pagination'}
+        // activeClassName={'active'}
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageClassName={'bg-blue-500 rounded-full py-2 px-4 mx-2 mb-10'}
+        activeClassName={'bg-[gray] text-white'}
+        containerClassName={'flex justify-center mt-4'}
+        previousClassName={'rounded-full py-2 px-4 mx-2'}
+        nextClassName={'rounded-full py-2 px-4 mx-2'}
+      />
       </div>
       <PrimaryButton text="Add product" onClick={() => navigate("add")} className="mb-16" />
     </div>
